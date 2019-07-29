f579bfe466b4e8e8cc9d379aceafbc50c4ec7d4d
luo <820185535@qq.com>
2019-07-29 10:38:16 +0800

    fix bug

--- app/src/main/java/com/sup/itg/itgbase/MainActivity.java
+++ app/src/main/java/com/sup/itg/itgbase/MainActivity.java
@@ -2,12 +2,29 @@ package com.sup.itg.itgbase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Message;
 
-public class MainActivity extends AppCompatActivity {
+import com.sup.itg.base.ItgHandlerActivity;
+
+public class MainActivity extends ItgHandlerActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+
     }
 }
