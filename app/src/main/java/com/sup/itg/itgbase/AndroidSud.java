ce30d197924513a1bea20826b335f48212d82629
luo <820185535@qq.com>
2019-08-05 15:34:44 +0800

    加入注解jar

---
+++ app/src/main/java/com/sup/itg/itgbase/AndroidSud.java
@@ -0,0 +1,13 @@
+package com.sup.itg.itgbase;
+
+import lzr.bn.Seriable;
+
+public class AndroidSud {
+    @Seriable
+    private String username;
+    @Seriable
+    private String password;
+
+    private String three;
+    private String four;
+}
--- app/src/main/java/com/sup/itg/itgbase/MainActivity.java
+++ app/src/main/java/com/sup/itg/itgbase/MainActivity.java
@@ -1,18 +1,17 @@
 package com.sup.itg.itgbase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Message;
 import android.view.View;
 
 import com.sup.itg.base.ItgHandlerActivity;
-import com.sup.itg.base.an.intent.IntentKey;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import lzr.bn.ApiAnnotation;
+
+
+@ApiAnnotation(author = "罗志然",date = "2019",version = 1)
 public class MainActivity extends ItgHandlerActivity {
 
     @Override
